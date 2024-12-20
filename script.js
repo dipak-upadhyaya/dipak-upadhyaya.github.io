665a78c67b1b8fc8e9db74ffba00ea71a0519997
dipak-upadhyaya <dipakprasad.upadhyaya@gmail.com>
2024-12-20 01:26:42 -0500

    Formatted About and Publications sections

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // Fetch the JSON file containing publications
 fetch('data/publications.json')
     .then(response => {
-        console.log('Fetching publications.json...');
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         return response.json();
     })
     .then(data => {
-        console.log('Publications data fetched successfully:', data);
-        const publicationsList = document.getElementById('publications-list');
+        const publicationsContainer = document.getElementById('publications-container');
         data.forEach(pub => {
-            const li = document.createElement('li');
-            li.innerHTML = `
-                <strong>${pub.title}</strong><br>
-                <em>${pub.authors}</em><br>
-                <span>${pub.journal} (${pub.year})</span><br>
-                ${pub.link ? `<a href="${pub.link}" target="_blank">Read More</a>` : ''}
+            const pubDiv = document.createElement('div');
+            pubDiv.classList.add('publication');
+
+            pubDiv.innerHTML = `
+                <h3>${pub.title}</h3>
+                <p class="authors">${pub.authors}</p>
+                <p class="journal">${pub.journal} <span class="year">${pub.year}</span></p>
+                <p>${pub.link ? `<a href="${pub.link}" target="_blank">Read More</a>` : ''}</p>
             `;
-            publicationsList.appendChild(li);
+            publicationsContainer.appendChild(pubDiv);
         });
     })
     .catch(error => console.error('Error fetching publications:', error));
