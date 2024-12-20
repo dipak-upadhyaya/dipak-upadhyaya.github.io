e2a47a94aadee19528bbe54ede4f332e1ea2308e
dipak-upadhyaya <dipakprasad.upadhyaya@gmail.com>
2024-12-20 02:10:22 -0500

    Updated icons and screen dyanmics

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+// Fetch the JSON file containing publications
 fetch('data/publications.json')
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
     .then(data => {
         const publicationsContainer = document.getElementById('publications-container');
         data.forEach(pub => {
-            const div = document.createElement('div');
-            div.classList.add('publication');
-            div.innerHTML = `
+            const pubDiv = document.createElement('div');
+            pubDiv.innerHTML = `
                 <strong>${pub.title}</strong><br>
                 <em>${pub.authors}</em><br>
                 <span>${pub.journal} (${pub.year})</span><br>
                 ${pub.link ? `<a href="${pub.link}" target="_blank">Read More</a>` : ''}
             `;
-            publicationsContainer.appendChild(div);
+            publicationsContainer.appendChild(pubDiv);
         });
     })
     .catch(error => console.error('Error fetching publications:', error));
