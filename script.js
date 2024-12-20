c5966c87a7b719922bffbc0f4f776a830efc1671
dipak-upadhyaya <dipakprasad.upadhyaya@gmail.com>
2024-12-20 00:59:29 -0500

    Fix publications display issue

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+// Fetch the JSON file containing publications
 fetch('data/publications.json')
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
     .then(data => {
-        console.log(data); // or dynamically populate the HTML
-    });
+        const publicationsList = document.getElementById('publications-list');
+        data.forEach(pub => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+                <strong>${pub.title}</strong><br>
+                <em>${pub.authors}</em><br>
+                <span>${pub.journal} (${pub.year})</span><br>
+                ${pub.link ? `<a href="${pub.link}" target="_blank">Read More</a>` : ''}
+            `;
+            publicationsList.appendChild(li);
+        });
+    })
+    .catch(error => console.error('Error fetching publications:', error));
+
+    <script src="script.js"></script>
+```
