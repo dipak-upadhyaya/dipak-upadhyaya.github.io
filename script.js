d505ae92f008f785886a5544906113fae3464287
dipak-upadhyaya <dipakprasad.upadhyaya@gmail.com>
2024-12-20 01:57:46 -0500

    Updated icons

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-// Fetch the JSON file containing publications
 fetch('data/publications.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
         const publicationsContainer = document.getElementById('publications-container');
         data.forEach(pub => {
-            const pubDiv = document.createElement('div');
-            pubDiv.className = 'publication';
-
-            pubDiv.innerHTML = `
-                <h3>${pub.title}</h3>
-                <p class="authors">${pub.authors}</p>
-                <p>${pub.journal} (${pub.year})</p>
+            const div = document.createElement('div');
+            div.classList.add('publication');
+            div.innerHTML = `
+                <strong>${pub.title}</strong><br>
+                <em>${pub.authors}</em><br>
+                <span>${pub.journal} (${pub.year})</span><br>
                 ${pub.link ? `<a href="${pub.link}" target="_blank">Read More</a>` : ''}
             `;
-            publicationsContainer.appendChild(pubDiv);
+            publicationsContainer.appendChild(div);
         });
     })
     .catch(error => console.error('Error fetching publications:', error));
