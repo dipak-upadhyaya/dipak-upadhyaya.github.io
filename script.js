947b9b56dc98c496d01bd35feb42bb22db5dbe1d
dipak-upadhyaya <dipakprasad.upadhyaya@gmail.com>
2024-12-20 10:20:37 -0500

    Corrected CV and publications

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ fetch('data/publications.json')
     .then(data => {
         const publicationsContainer = document.getElementById('publications-container');
         data.forEach(pub => {
-            const li = document.createElement('li');
+            const card = document.createElement('div');
+            card.classList.add('publication-card');
             const boldedAuthors = pub.authors.replace(/Upadhyaya, D\. P\./g, '<strong>Upadhyaya, D. P.</strong>'); // Bold the name
-            li.innerHTML = `
+            card.innerHTML = `
                 <p><strong>${pub.title}</strong></p>
                 <p><em>${boldedAuthors}</em></p>
                 <p>${pub.journal} (${pub.year})</p>
                 ${pub.link ? `<p><a href="${pub.link}" target="_blank">Read More</a></p>` : ''}
             `;
-            publicationsContainer.appendChild(li);
+            publicationsContainer.appendChild(card);
         });
     })
     .catch(error => console.error('Error fetching publications:', error));
